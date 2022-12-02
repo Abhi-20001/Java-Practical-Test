5258c45a3378edf5df70f8b50d287b90d9c8aa39
Abhi-20001 <abhichinchawade24@gmail.com>
2022-12-02 15:05:14 +0530

    Version-2

--- 1-Dec-2022-PracticalTest/src/com/mygame/main/Main.java
+++ 1-Dec-2022-PracticalTest/src/com/mygame/main/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		
 		String name[] = view.displayCityNamesWithBlanks();
 		
-		for(int i = 0 ; i <=5 ; i++)
+		for(int i = 0 ; i <=4 ; i++)
 		System.out.println(name[i]);
 		
 		
--- 1-Dec-2022-PracticalTest/src/com/mygame/view/GameView.java
+++ 1-Dec-2022-PracticalTest/src/com/mygame/view/GameView.java
@@ -17,13 +17,12 @@ public class GameView {
 			int num = c[i].length();
 			int k = 0;
 			// int randomArray[]= null;
+			char arr[]= c[i].toCharArray();
 			
 			while(k != 3)
 			{
 				int val = v.generateRandom(num);
 				
-				char arr[]= c[i].toCharArray();
-
 				for(int j=0; j < arr.length;j++)
 				{
 					if( i == val)
@@ -31,23 +30,27 @@ public class GameView {
 						arr[i] = ' ';
 					}		
 				}
-				c[i] = String.valueOf(arr);
+				
 				k++;
 			}
+			c[i] = String.valueOf(arr);
 		}
 		return c;
 	}
 	
 	
 	
-	int generateRandom(int n)
+	int[] generateRandom(int n)
 	{
 		int max = n;
 		int min = 1;
+		int i = 0;
 		
 		java.util.Random random = new java.util.Random();
-		
-		int value = random.nextInt(max - min) + min;
+		while(i != max/3 )
+		{
+			int value[i] = (int) (random.nextInt(max - min) + min);
+		}
 		return value;
 	}
 
